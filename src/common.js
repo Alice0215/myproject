fa15afcdcd04a7a41e8ba5196436fde572da902d
Vitta <wanglikun@shinetechchina.com>
2018-03-28 17:55:55 +0800

    拍完照片 调试对接 上传文件 接口

--- src/common.js
+++ src/common.js
@@ -1,6 +1,8 @@
 import { server } from './const'
 import axios from 'axios'
 import store from './store'
+import _ from 'lodash'
+
 async function request (url, method = 'get', data = {}, responseType = 'json', project = false) {
   const endpoint = project ? server.PROJECT_API : server.API
   url = endpoint + url
@@ -27,6 +29,32 @@ async function request (url, method = 'get', data = {}, responseType = 'json', p
   })
   return resp
 }
+
+/**
+ * Base64 转 blob
+ * @param dataurl
+ * @param filename
+ * @returns {File}
+ */
+function dataURLtoFile (dataurl, filename = Date.now() + '.jpeg') {
+  let arr = dataurl.split(',')
+  let mime, bstr, n, u8arr
+  if (_.isNull(arr[0].match(/:(.*?);/))) {
+    mime = 'image/jpeg'
+    bstr = atob(arr[0])
+  } else {
+    mime = arr[0].match(/:(.*?);/)[1]
+    bstr = atob(arr[1])
+  }
+  n = bstr.length
+  u8arr = new Uint8Array(n)
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n)
+  }
+  return new File([u8arr], filename, {type: mime})
+}
+
 export {
-  request
+  request,
+  dataURLtoFile
 }
