82e44f7bdd595d6e6fd4de0910074e8d9e335815
xtq <xuetq@shinetechchina.com>
2018-04-13 21:25:03 +0800

    修改变更

---
+++ src/const.js
@@ -0,0 +1,22 @@
+const host = 'http://60.195.68.29:3535/'
+
+const server = {
+  HOST: host,
+  API: host + 'landscape/cm/',
+  PROJECT_API: host + 'landscape/ls/',
+  THUMBNAIL_QR: host + 'fs/ls/qr/',
+  THUMBNAIL_API: host + 'fs/ls/thumbnail/',
+  PREVIEW_API: host + 'fs/ls/uploaded/'
+}
+
+const plantType = {
+  SINGLE: 'SINGLE',
+  AREA: 'AREA',
+  DEVICE: 'EQUIPMENT',
+  OTHER: 'OTHER'
+}
+
+export {
+  server,
+  plantType
+}
