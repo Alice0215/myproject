b286f7be560518351076286345621655b68a01b7
Vitta <wanglikun@shinetechchina.com>
2018-05-03 16:19:54 +0800

    修复 vivo下 「我的项目」列表页面头部菜单栏不固定

--- src/pages/project/list.vue
+++ src/pages/project/list.vue
@@ -1,132 +1,138 @@
 <template>
-    <q-list class="list" id="project-list">
-        <q-toolbar class="header">
-        <q-toolbar class="fix">
-            <q-toolbar-title class="header-title">
-            项目
-            </q-toolbar-title>
-       </q-toolbar>
-        </q-toolbar>
-        <q-item class="list-list"  @click.native="$router.push('project/add')" >
-             <span class="add-btn">+</span>
-            <q-item-main class="add-field">
+  <q-layout>
+    <q-layout-header>
+      <q-toolbar class="fix">
+        <q-toolbar-title class="header-title">
+          项目
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-header>
+    <q-page-container>
+      <q-list class="list" id="project-list">
+        <q-item class="list-list mt-40" @click.native="$router.push('project/add')">
+          <span class="add-btn">+</span>
+          <q-item-main class="add-field">
             创建项目
-            </q-item-main>
+          </q-item-main>
         </q-item>
         <q-infinite-scroll :handler="load" class="scroll-field">
-        <q-item multiline  v-for="item in list"
-          :key="item.id" class="list-list"  @click.native="$router.push('qcode/list?projectId='+item.id)">
-           <q-item-side class="add-btn" />
+          <q-item multiline v-for="item in list"
+                  :key="item.id" class="list-list" @click.native="$router.push('qcode/list?projectId='+item.id)">
+            <q-item-side class="add-btn"/>
             <q-item-main>
-            <q-item-tile label class="title">{{item.projectName}}</q-item-tile>
-            <q-item-tile sublabel lines="2" class="content">
-            {{item.projectDesc}}
-            </q-item-tile>
+              <q-item-tile label class="title">{{item.projectName}}</q-item-tile>
+              <q-item-tile sublabel lines="2" class="content">
+                {{item.projectDesc}}
+              </q-item-tile>
             </q-item-main>
-        </q-item>
-        <div class="row justify-center" style="margin-bottom: 50px;" v-if="!hasLoadAll">
-          <q-spinner name="dots" slot="message" :size="40"></q-spinner>
-        </div>
-      </q-infinite-scroll>
-      <q-tabs class="footer">
-        <q-route-tab slot="title" icon="dashboard" to="/" replace label="我的项目" class="menu" />
-        <q-route-tab slot="title" icon="view_array" to="/qcode/scan" append label="扫二维码" class="menu"/>
-        <q-route-tab slot="title" icon="event note" to="/" replace label="巡查" class="menu"/>
-        <q-route-tab slot="title" icon="person" to="/jobGroup/byUser" replace label="我的" class="menu"/>
-      </q-tabs>
-    </q-list>
+          </q-item>
+          <div class="row justify-center" style="margin-bottom: 50px;" v-if="!hasLoadAll">
+            <q-spinner name="dots" slot="message" :size="40"></q-spinner>
+          </div>
+        </q-infinite-scroll>
+        <q-tabs class="footer">
+          <q-route-tab slot="title" icon="dashboard" to="/" replace label="我的项目" class="menu"/>
+          <q-route-tab slot="title" icon="view_array" to="/qcode/scan" append label="扫二维码" class="menu"/>
+          <q-route-tab slot="title" icon="event note" to="/" replace label="巡查" class="menu"/>
+          <q-route-tab slot="title" icon="person" to="/jobGroup/byUser" replace label="我的" class="menu"/>
+        </q-tabs>
+      </q-list>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
-import { request } from '../../common'
-export default {
-  data () {
-    return {
-      list: '',
-      loading: false,
-      pageNo: 1,
-      hasLoadAll: false
-    }
-  },
-  mounted () {
-  },
-  methods: {
-    toDetail (projectId) {
-      return 'qcode/List?projectId' + projectId
+  import { request } from '../../common'
+
+  export default {
+    data () {
+      return {
+        list: '',
+        loading: false,
+        pageNo: 1,
+        hasLoadAll: false,
+      }
     },
-    async load (index, done) {
-      let that = this
-      setTimeout(() => {
-        if (!this.hasLoadAll) {
-          this.loading = true
-          request(
-            'project/list?pageNo=' + that.pageNo + '&pageSize=20',
-            'get',
-            '',
-            'json',
-            true
-          ).then(response => {
-            if (response.data.resultCode === 'SUCCESS') {
-              this.loading = false
-              let list = response.data.resultMsg
-              if (list.length === 0 || !list.length) {
-                that.hasLoadAll = true
-                return
+    mounted () {
+    },
+    methods: {
+      toDetail (projectId) {
+        return 'qcode/List?projectId' + projectId
+      },
+      async load (index, done) {
+        let that = this
+        setTimeout(() => {
+          if (!this.hasLoadAll) {
+            this.loading = true
+            request(
+              'project/list?pageNo=' + that.pageNo + '&pageSize=20',
+              'get',
+              '',
+              'json',
+              true,
+            ).then(response => {
+              if (response.data.resultCode === 'SUCCESS') {
+                this.loading = false
+                let list = response.data.resultMsg
+                if (list.length === 0 || !list.length) {
+                  that.hasLoadAll = true
+                  return
+                }
+                if (list.length < 20) {
+                  that.hasLoadAll = true
+                } else {
+                  that.pageNo++
+                }
+                if (that.list.length > 0) {
+                  that.list = that.list.concat(list)
+                } else {
+                  that.list = list
+                }
+                done()
               }
-              if (list.length < 20) {
-                that.hasLoadAll = true
-              } else {
-                that.pageNo++
-              }
-              if (that.list.length > 0) {
-                that.list = that.list.concat(list)
-              } else {
-                that.list = list
-              }
-              done()
-            }
-          })
-        }
-      }, 100)
-    }
+            })
+          }
+        }, 100)
+      },
+    },
   }
-}
 </script>
 
 <style lang='scss'>
-.list {
+  .list {
     padding: 0px;
     border: none;
     background: none;
   }
-@import "../../assets/css/common";
-#project-list {
 
-  .scroll-field {
-    margin-bottom: 63px;
+  @import "../../assets/css/common";
+  #project-list {
+
+    .scroll-field {
+      margin-bottom: 63px;
+    }
+    .add-btn {
+      width: 75px;
+      height: 75px;
+      background-color: #dcdcdc;
+      border-radius: 5px;
+      color: white;
+      font-size: 60px;
+      line-height: 68px;
+      text-align: center;
+    }
+    .add-field {
+      padding-left: 10px;
+    }
+    .title {
+      font-size: 16px;
+      color: black;
+      padding-top: 10px;
+    }
+    .content {
+      font-size: 14px;
+      color: #666666 !important;
+      margin-top: 5px;
+    }
   }
-  .add-btn {
-    width: 75px;
-    height: 75px;
-    background-color: #dcdcdc;
-    border-radius: 5px;
-    color: white;
-    font-size: 60px;
-    line-height: 68px;
-    text-align: center;
-  }
-  .add-field {
-    padding-left: 10px;
-  }
-  .title {
-    font-size: 16px;
-    color: black;
-    padding-top: 10px;
-  }
-  .content {
-    font-size: 14px;
-    color: #666666 !important;
-    margin-top: 5px;
-  }
-}
 </style>
