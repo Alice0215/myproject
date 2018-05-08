109a9964b84208ba2ba2a7c736fefdf3f90e1e96
Vitta <wanglikun@shinetechchina.com>
2018-05-08 15:51:16 +0800

    使用 iconfont 添加 图标

--- src/pages/choose/chooseQRType.vue
+++ src/pages/choose/chooseQRType.vue
@@ -15,21 +15,25 @@
     <div class="mt-40 text-center font-16">请选择需要录入的类型</div>
     <q-card class="mt-20 shadow-1" @click.native="singleCardClicked">
       <q-card-main>
+        <i class="iconfont">&#xe64c;</i>
         <label>单株植物</label>
       </q-card-main>
     </q-card>
     <q-card class="mt-16 shadow-1" @click.native="areaCardClicked">
       <q-card-main>
+        <i class="iconfont">&#xe909;</i>
         <label>片区植物</label>
       </q-card-main>
     </q-card>
     <q-card class="mt-16 shadow-1" @click.native="deviceCardClicked">
       <q-card-main>
+        <i class="iconfont">&#xe62f;</i>
         <label>设备</label>
       </q-card-main>
     </q-card>
     <q-card class="mt-16 shadow-1" @click.native="otherCardClicked">
       <q-card-main>
+        <i class="iconfont">&#xe64b;</i>
         <label>其他</label>
       </q-card-main>
     </q-card>
@@ -75,6 +79,11 @@ export default {
         font-size: 18px;
         font-weight: bold;
         color: #727272;
+      }
+
+      i {
+        color: #a0da96;
+        font-size: 20px;
       }
     }
   }
