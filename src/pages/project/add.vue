f5a4bba08b12b9d316174b59de4de18eda74346b
xtq <xuetq@shinetechchina.com>
2018-05-09 12:43:49 +0800

    添加数据处理

--- src/pages/project/add.vue
+++ src/pages/project/add.vue
@@ -31,102 +31,114 @@
       <q-search icon="place" color="amber" v-model="formData.address" @click="openMap"
                 class="login-input" disable  placeholder="输入地址/定位地址"/>
       </q-field>
-      <q-input type="textarea" v-model="formData.projectDesc" class="login-input" placeholder="项目简介"/>
-        <q-item link class="full-width underline users" @click.native="chooseUser('TM')">
-            <q-item-main :label="`设置参与者`" /><span class="user" v-for="TMitem in formData.TMlable" v-bind:key="TMitem.id" >{{TMitem}}</span>
-            <q-item-side right icon="keyboard_arrow_right" />
-        </q-item>
-         <q-field
-         @blur="$v.formData.TLSelect.$touch"
-        @keyup.enter="add"
-        :error="$v.formData.TLSelect.$error"
-         error-label="请项目设置负责人">
-          <q-item link class="full-width underline users"  @click.native="chooseUser('TL')" >
-              <q-item-main :label="`设置负责人`" /><span class="user"  v-for="TLitem in formData.TLlable" v-bind:key="TLitem.id">{{TLitem}}</span>
-              <q-item-side right  icon="keyboard_arrow_right" />
-          </q-item>
-         </q-field>
+       <q-item link class="full-width underline users" @click.native="chooseProjectType">
+          <q-item-side> 项目类型</q-item-side>
+          <q-item-main class="text-right" v-line-clamp:20="1">
+            {{ selectProjectType ? selectProjectType.name : ''}}
+          </q-item-main>
+          <q-item-side right icon="expand more" />
+      </q-item>
+        <q-field
+              @blur="$v.formData.managerUsers.$touch"
+              @keyup.enter="add"
+              :error="$v.formData.managerUsers.$error"
+              error-label="请添加项目负责人">
+              <q-item class="underline" @click.native="chooseUser('TL')" >
+                <q-item-side>  项目负责人</q-item-side>
+                <q-item-main class="text-right" v-line-clamp:20="1">
+                  <q-item-tile>
+                    <span v-for="v in formData.managerUsers" :key="v.userId"
+                          icon-right="clear">
+                    {{ v.fullname }}</span>
+                  </q-item-tile>
+                </q-item-main>
+                <q-item-side right  icon="expand more" />
+              </q-item>
+            </q-field>
+            <q-field
+              @blur="$v.formData.memberUsers.$touch"
+              @keyup.enter="add"
+              :error="$v.formData.memberUsers.$error"
+              error-label="请添加项目成员">
+              <q-item class="underline"  @click.native="chooseUser('TM')">
+                <q-item-side>  项目成员</q-item-side>
+                <q-item-main class="text-right" v-line-clamp:20="1">
+                  <q-item-tile>
+                    <span v-for="v in formData.memberUsers" :key="v.userId">
+                      {{ v.fullname }}
+                  </span>
+                  </q-item-tile>
+                </q-item-main>
+                <q-item-side right icon="expand more" />
+              </q-item>
+            </q-field>
+         <q-input type="textarea" v-model="formData.projectDesc" class="login-input" placeholder="项目简介"/>
     </div>
     </q-page>
     </q-page-container>
   </q-layout>
+  <q-modal v-model="selectProjectTypeOpen" :content-css="{minWidth: '80vw', minHeight: '80vh'}" position="bottom">
+      <q-modal-layout id="choose-operator-detail">
+        <q-toolbar slot="header">
+          <q-item-side v-close-overlay class="font-14">取消</q-item-side>
+          <q-toolbar-title class="header-title">
+            选择项目类型
+          </q-toolbar-title>
+        </q-toolbar>
+        <q-search v-model="search" icon="search" type="text" class="m-10" clearable placeholder="搜索"/>
+        <hr>
+        <q-list no-border>
+          <q-item v-for="type in projectTypes" :key="type.id" v-if="type.show !== false"
+                  @click.native="chooseType(type)"
+                  :class="{'text-main-color bg-white': type.checked}">
+            <q-item-main :label="type.name" />
+            <q-item-side right icon="done" class="text-main-color" v-if="type.checked" />
+          </q-item>
+        </q-list>
+      </q-modal-layout>
+    </q-modal>
+    <q-modal v-model="opened" :content-css="{minWidth: '80vw', minHeight: '80vh'}" position="bottom">
+      <q-modal-layout>
+        <q-toolbar slot="header">
+          <q-item-side v-close-overlay class="font-14">关闭</q-item-side>
+          <q-toolbar-title class="header-title">
+          </q-toolbar-title>
+          <q-item-side class="font-14" v-close-overlay  @click.native="completed" right>完成</q-item-side>
+        </q-toolbar>
+        <q-search v-model="search" icon="search" type="text" class="m-10" clearable placeholder="搜索"/>
+        <q-list-header>{{ jobType === 'TL' ? '已选负责人' : '已选成员' }}</q-list-header>
+        <div class="m-10">
+          <q-chip v-for="(v, i) in formData.selectedUsers" :key="v.userId" @click="removeUser(v, i, null)" icon-right="clear">
+            {{ v.fullname }}
+          </q-chip>
+        </div>
+        <q-list inset-separator no-border>
+          <q-list-header>{{ '人员列表(' + count + ')'}}</q-list-header>
+          <q-item v-for="user in users" :key="user.userId" v-if="user.show !== false">
+            <q-item-side icon="account circle" class="user"/>
+            <q-item-main :label="user.fullname" />
+            <q-item-side>
+              <q-checkbox v-model="user.checked" @input="choose(user)" />
+            </q-item-side>
+          </q-item>
+        </q-list>
+      </q-modal-layout>
+    </q-modal>
   </div>
 </template>
 
 <script>
-import { required } from 'vuelidate/lib/validators'
 import { request } from '../../common'
-// import eventBus from '../../eventBus'
+import ProjectMixin from '../../mixin/ProjectMixin'
 export default {
+  mixins: [
+    ProjectMixin
+  ],
   data () {
     return {
-      formData: {
-        projectName: '',
-        projectDesc: '',
-        address: '',
-        locationJson: '',
-        TMlable: [],
-        TLlable: [],
-        TMSelect: [],
-        TLSelect: [],
-        TMobg: [],
-        TLobg: [],
-        geoInfo: null
-      },
-      tempType: ''
-    }
-  },
-  validations: {
-    formData: {
-      projectName: { required },
-      locationJson: { required },
-      TLSelect: { required }
     }
   },
   created () {
-    if (localStorage.getItem('oldInfo') && localStorage.getItem('oldInfo') !== 'undefined') {
-      let oldInfo = JSON.parse(localStorage.getItem('oldInfo'))
-      this.formData = oldInfo
-      localStorage.removeItem('oldInfo')
-    }
-    if (this.$route.query.user) {
-      if (this.$route.query.type === 'TM') {
-        this.formData.TMobg = []
-        this.formData.TMlable = []
-        this.formData.TMSelect = []
-      } else {
-        this.formData.TLobg = []
-        this.formData.TLlable = []
-        this.formData.TLSelect = []
-      }
-      for (var val of JSON.parse(this.$route.query.user)) {
-        let obg = {
-          'jobType': '',
-          'userId': ''
-        }
-        let userInfo = { 'fullname': val.fullname, 'userId': val.userId }
-        obg.userId = val.userId
-        if (this.$route.query.type === 'TM') {
-          obg.jobType = 'TM'
-          if (this.formData.TMobg.indexOf(obg) === -1) {
-            if (this.formData.TMobg.length < 3) {
-              this.formData.TMlable.push(val.fullname)
-            }
-            this.formData.TMSelect.push(userInfo)
-            this.formData.TMobg.push(obg)
-          }
-        } else {
-          obg.jobType = 'TL'
-          if (this.formData.TLobg.indexOf(obg) === -1) {
-            if (this.formData.TLobg.length < 3) {
-              this.formData.TLlable.push(val.fullname)
-            }
-            this.formData.TLSelect.push(userInfo)
-            this.formData.TLobg.push(obg)
-          }
-        }
-      }
-    }
     if (localStorage.getItem('user_location') !== '') {
       this.formData.geoInfo = JSON.parse(localStorage.getItem('user_location'))
       if (this.formData.geoInfo !== null) {
@@ -137,6 +149,11 @@ export default {
     }
   },
   methods: {
+    back () {
+      // this.$store.commit('WorkOrder/setInfo', null)
+      // this.$store.commit('WorkOrder/setJobs', null)
+      this.$router.goBack(this.isEdited, '确认放弃创建项目吗？', '离开当前页面您的项目信息将不会保存')
+    },
     async add () {
       this.$v.formData.$touch()
       if (this.$v.formData.$error) {
@@ -156,22 +173,14 @@ export default {
       }
       await request('project/create', 'post', data, 'json', true).then(response => {
         if (response && response.data.resultCode === 'SUCCESS') {
-          this.$q.dialog({
-            title: '提示',
+          this.$q.notify({
+            timeout: 2000,
+            type: 'positive',
             message: '项目添加成功！'
           })
-          this.$router.push('/')
+          this.$router.goBack()
         }
       })
-    },
-    chooseUser (jobType) {
-      localStorage.setItem('oldInfo', JSON.stringify(this.formData))
-      if (jobType === 'TM') {
-        localStorage.setItem('selectedUser', JSON.stringify(this.formData.TMSelect))
-      } else {
-        localStorage.setItem('selectedUser', JSON.stringify(this.formData.TLSelect))
-      }
-      this.$router.push('allUser?type=' + jobType)
     },
     openMap () {
       localStorage.setItem('oldInfo', JSON.stringify(this.formData))
@@ -190,7 +199,6 @@ export default {
       padding: 3px;
       text-align: center;
       display: inline-block;
-      background-color: #dcdcdc;
       border-radius: 2px;
       margin-left: 3px;
     }
