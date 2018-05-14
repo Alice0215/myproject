55554ec00018bbf1d712b081b37186d778d110bb
luyang <lu.yang.end@gmail.com>
2018-05-14 19:45:41 +0800

    养护记录review

--- src/pages/project/MaintenanceRecords.vue
+++ src/pages/project/MaintenanceRecords.vue
@@ -2,7 +2,7 @@
   <q-layout view="Hhh lpr Fff">
     <q-page-container>
       <q-page id="maintenance_records">
-        <q-infinite-scroll :handler="getJobGroup">
+        <q-infinite-scroll :handler="load">
           <q-list separator v-if="joblist.length > 0">
             <q-item v-for="item in joblist"
                     :key="item.id"
@@ -18,7 +18,7 @@
                   <div class="pv-4 row">
                     <div class="work-content-title">工作内容：</div>
                     <div class="work-content" v-line-clamp:20="1">
-                    <span v-for="v in item.jobs" v-if="item.jobs">
+                    <span v-for="v in item.jobs" v-if="item.jobs" :key="v.id">
                       <span v-if="v.action" class="ml-5">{{ v.action.name }}</span>
                     </span>
                     </div>
@@ -69,40 +69,36 @@ export default {
     }
   },
   methods: {
-    load (index, done) {
-      this.$q.loading.show()
-      request('jobGroup/list/byProject?projectId=' + this.projectId + '&pageNo=' + this.pageNo + '&pageSize=20', 'get', null, 'json', true).then(response => {
-        this.$q.loading.hide()
-        if (response.data.resultCode === 'SUCCESS') {
-          let that = this
-          let list = response.data.resultMsg
-          if (list.length === 0 || !list.length) {
-            this.hasLoadAll = true
-            return
-          }
-          if (list.length < 20) {
-            that.hasLoadAll = true
-          } else {
-            that.pageNo++
-            this.hasLoadAll = false
-          }
-          if (that.joblist.length > 0) {
-            that.joblist = that.joblist.concat(list)
-          } else {
-            that.joblist = list
-          }
-          if (this.joblist.length > 0) {
-            eventBus.$emit('has-maintenance-records')
-          }
-          this.$forceUpdate()
-          done()
+    
+    async load (index, done) {
+      let that = this
+      setTimeout(() => {
+        if (!that.hasLoadAll) {
+          request('jobGroup/list/byProject?projectId=' + that.projectId + '&pageNo=' + that.pageNo + '&pageSize=20', 'get', '', 'json', true).then(response => {
+            if (response) {
+              let list = response.data.resultMsg
+              if (list.length === 0 || !list.length) {
+                that.hasLoadAll = true
+                return
+              }
+              if (list.length < 20) {
+                that.hasLoadAll = true
+              } else {
+                that.pageNo++
+              }
+              if (that.joblist.length > 0) {
+                that.joblist = that.joblist.concat(list)
+              } else {
+                that.joblist = list
+              }
+              if (this.joblist.length > 0) {
+                eventBus.$emit('has-maintenance-records')
+              }
+              done()
+            }
+          })
         }
-      })
-    },
-    getJobGroup (index, done) {
-      if (!this.hasLoadAll) {
-        this.load(index, done)
-      }
+      }, 100)
     }
   }
 }
