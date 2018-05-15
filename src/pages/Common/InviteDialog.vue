247aef05bf47f49bc71685f0cc0a365b75926b8b
Faye <muyf@shinetechchina.com>
2018-05-15 18:03:57 +0800

    1添加成员管理界面 2修改网络请求api

---
+++ src/pages/Common/InviteDialog.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-modal v-model="selectYear" :content-css="{minWidth: '100vw', minHeight: '253px'}" position="bottom">
+    <q-modal-layout>
+      <q-toolbar slot="header">
+        <q-item-side v-close-overlay class="font-14 text-main-color">取消</q-item-side>
+        <q-toolbar-title class="header-title">
+          添加成员
+        </q-toolbar-title>
+        <q-item-side v-close-overlay class="font-14 text-main-color"></q-item-side>
+      </q-toolbar>
+    </q-modal-layout>
+  </q-modal>
+</template>
+
+<script>
+  import eventBus from '../../eventBus'
+  import { requestAdmin } from '../../common'
+
+  export default {
+    data () {
+      return {
+      }
+    },
+    methods: {
+    },
+    mounted () {
+    }
+  }
+</script>
+
+<style>
+</style>
