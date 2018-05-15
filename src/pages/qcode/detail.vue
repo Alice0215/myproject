eb2d49ae46c2461a6dc770e010328fe3a932b59b
xtq <xuetq@shinetechchina.com>
2018-05-15 16:06:05 +0800

    二维码信息页完成

--- src/pages/qcode/detail.vue
+++ src/pages/qcode/detail.vue
@@ -1,70 +1,71 @@
 <template>
-  <div id="detail">
-    <q-toolbar class='header'>
-        <q-toolbar class='fix'>
-             <a @click="back" class='back-a'><q-item-side left  icon='keyboard arrow left' class='back-left'/>返回</a>
-            <q-toolbar-title class='header-title'>
-              {{topTitle}}
-            </q-toolbar-title>
-            <a class="top-nav-right" @click="$router.push(routeUrl)">{{qrRecord}}</a>
-       </q-toolbar>
-    </q-toolbar>
-    <div class='full-width card'>
-      <div class="top-field">
-        <p class='text-center'>
-          <img v-bind:src="qrImgUrl" v-preview="qrImgUrl" preview-nav-enable="false" class="qr-img">
-        </p>
-        <p class='text-center'>
-          <q-btn class="show-qr" @click="goToView">查看二维码</q-btn>
-        </p>
-      </div>
-      <div class="qr-info">
+ <q-layout view="Hhh lpr Fff" class="bg-primary" id="qr-detail">
+    <q-layout-header>
+      <q-toolbar>
+          <a @click="this.$router.goBack()" class="back-a font-14">
+          <q-item-side left  icon="keyboard arrow left" class="back-left "/>
+          返回
+        </a>
+        <q-toolbar-title class="header-title" v-if="info.alias">
+          {{info.alias}}
+        </q-toolbar-title>
+         <q-toolbar-title class="header-title" v-if="info.code && info.code.alias">
+          {{info.code.alias}}
+        </q-toolbar-title>
+         <q-item-side class="white-right" right/>
+      </q-toolbar>
+      <q-tabs inverted align="justify" no-pane-border>
+        <q-tab default name="maintenance-records" slot="title" label="植物信息"  class="mt-5 pb-0" />
+        <q-tab slot="title" label="养护记录" class="mt-5 pb-0"/>
+        <q-tab slot="title" label="巡查记录" class="mt-5 pb-0"/>
+      </q-tabs>
+    </q-layout-header>
+    <q-page-container class="bg-white box">
+      <div class="qr-info pv-15">
         <div v-if="type==='EQUIPMENT' || type==='OTHER'">
-          <p>名称：<span v-if="info.alias">{{info.alias}}</span></p>
-          <p>所属项目：<span v-if="info.project">{{info.project.projectName}}</span></p>
-          <p class="address"> <q-item-side left icon='place' class='inline newicon'></q-item-side><span  v-if="info.location">{{info.location.formattedAddress}}</span></p>
-          <p class="address" v-if="info.description"> {{info.description}}</p>
-          <p class="address pic-field" v-if=" info.pictures">
+          <p><span class="wp-30 ib title-font-color">名称：</span><span v-if="info.alias"  v-line-clamp:20="1" class="ib wp-70 float-right">{{info.alias}}</span></p>
+          <p><span class="wp-30 ib title-font-color">所属项目：</span><span v-if="info.project" v-line-clamp:20="1" class="wp-70 ib float-right">{{info.project.projectName}}</span></p>
+           <p><span class="wp-30 ib title-font-color">地址：</span><span v-if="info.location" v-line-clamp:20="1" class="wp-70 ib float-right">{{info.location.formattedAddress}}</span></p>
+          <p v-if="info.description"><span class="block title-font-color">备注：</span>{{info.description}}</p>
+          <p class="pic-field" v-if=" info.pictures">
             <span v-for="item in info.pictures" v-bind:key="item.id">
               <img :src="picUrl+item.filePath" v-preview="previewApi+item.filePath"/>
             </span>
           </p>
         </div>
         <div v-if="type==='SINGLE'|| type==='AREA'">
-        <p>二维码编号：<span v-if="info.code && info.code.identifier">{{info.code.identifier}}</span></p>
-        <p>二维码类型：<span v-if="info.code && info.code.type">{{info.code.type.value}}</span></p>
-        <p>所属项目：<span v-if="info.code && info.code.project">{{info.code.project.projectName}}</span></p>
-        </div>
-        <div v-if="type==='SINGLE'">
-        <p>植物名称：<span v-if="info.code && info.code.alias">{{info.code.alias}}</span></p>
-        <p>苗木分类：<span v-if="info.category">{{info.category.name}}</span></p>
-        <p class="param"><span>胸径：{{info.xiongJing}}cm</span><span>高度：{{info.gaoDu}}cm</span><span>冠幅：{{info.guanFu}}cm</span></p>
-        <p>苗木商名称：<span v-if="info.dealer">{{info.dealer}}</span></p>
-        <p>苗木其它信息：<span>{{info.other}}</span></p>
-        </div>
-        <div v-if="type==='AREA'">
-          <p>片区名称：<span v-if="info.code && info.code.alias">{{info.code.alias}}</span></p>
-          <p>片区面积：<span>{{info.acreage}}</span></p>
-          <p>植物:</p>
-          <p v-if="singles && singles.alias">
-          <q-chips-input v-model="singles.alias" hide-underline readonly chips-bg-color="lightGray" chips-color="black"/>
-        </p>
-        </div>
-        <div v-if="type==='SINGLE'|| type==='AREA'">
-          <p class="address"> <q-item-side left icon='place' class='inline newicon'></q-item-side><span  v-if="info.code && info.code.location">{{info.code.location.formattedAddress}}</span></p>
-          <p class="address" v-if="info.code && info.code.description"> {{info.code.description}}</p>
-          <p class="address pic-field" v-if="info.code && info.code.pictures">
+          <p><span class="wp-30 ib title-font-color">二维码编号：</span><span v-if="info.code && info.code.identifier">{{info.code.identifier}}</span></p>
+          <p><span class="wp-30 ib title-font-color">二维码类型：</span><span v-if="info.code && info.code.type" >{{info.code.type.value}}</span></p>
+          <p><span class="wp-30 ib title-font-color">所属项目：</span><span v-if="info.code && info.code.project" v-line-clamp:20="1" class="ib wp-70 float-right">{{info.code.project.projectName}}</span></p>
+          <p><span class="wp-30 ib title-font-color">地址：</span><span v-if="info.code && info.code.location" v-line-clamp:20="1" class="wp-70 ib float-right">{{info.code.location.formattedAddress}}</span></p>
+          <div v-if="type==='SINGLE'">
+          <p><span class="wp-30 ib title-font-color">植物名称：</span><span v-if="info.code && info.code.alias" class="ib wp-70 float-right"  v-line-clamp:20="1">{{info.code.alias}}</span></p>
+          <p><span class="wp-30 ib title-font-color">苗木分类：</span><span v-if="info.category">{{info.category.name}}</span></p>
+          <p class="param"><span class="wp-30 ib title-font-color">苗木规格：</span><span class="ib pr-5">胸径：{{info.xiongJing}}cm</span><span  class="ib pr-5">高度：{{info.gaoDu}}cm</span><span  class="ib pr-5">冠幅：{{info.guanFu}}cm</span></p>
+          <p><span class="wp-30 ib title-font-color">苗木商名称：</span><span v-if="info.dealer" class="ib wp-70 float-right"  v-line-clamp:20="1">{{info.dealer}}</span></p>
+          <p><span class="wp-30 ib title-font-color">苗木其它信息：</span><span class="ib wp-70 float-right"  v-line-clamp:20="1">{{info.other}}</span></p>
+          </div>
+          <div v-if="type==='AREA'">
+            <p>片区名称：<span v-if="info.code && info.code.alias" class="ib wp-70 float-right"  v-line-clamp:20="1">{{info.code.alias}}</span></p>
+            <p>片区面积：<span>{{info.acreage}}</span></p>
+            <p>植物:</p>
+            <p v-if="singles && singles.alias">
+              <q-chips-input v-model="singles.alias" hide-underline readonly chips-bg-color="lightGray" chips-color="black"/>
+            </p>
+          </div>
+          <p v-if="info.code && info.code.description"><span class="block title-font-color">备注：</span>{{info.code.description}}</p>
+          <p class="pic-field"  v-if="info.code && info.code.pictures">
+            <span class="block">现场图片：</span>
             <span v-for="item in info.code.pictures" v-bind:key="item.id">
               <img :src="picUrl+item.filePath" v-preview="previewApi+item.filePath"
-                   :key="item.id"
-                   preview-title-enable="false" />
+                    :key="item.id"
+                    preview-title-enable="false" />
             </span>
           </p>
-         </div>
-          <q-btn class="full-width bg-color qr-btn show-qr" v-if="editable" @click='add()'>编辑基础信息</q-btn>
+        </div>
       </div>
-    </div>
-  </div>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
@@ -75,13 +76,10 @@ export default {
     return {
       projectId: '',
       qrCodeId: '',
-      routeUrl: '',
       type: '',
       editable: false,
-      topTitle: '',
       qrImgUrl: '',
       info: {},
-      qrRecord: '',
       picUrl: '',
       previewApi: '',
       singles: {}
@@ -100,9 +98,6 @@ export default {
       localStorage.setItem('qrcode-preview', this.qrImgUrl)
       this.$router.push('/qcode/view')
     },
-    back () {
-      this.$router.goBack()
-    },
     add () {
       if (!this.editable) {
         this.$q.dialog({
@@ -119,46 +114,23 @@ export default {
       this.$q.loading.show()
       request('qrcode/detail?qrCodeId=' + this.qrCodeId, 'get', null, 'json', true).then(response => {
         this.$q.loading.hide()
-        if (response.data && response.data.resultCode === 'SUCCESS') {
+        if (response) {
           this.info = response.data.resultMsg
           if (this.type === 'SINGLE' || this.type === 'AREA') {
             if (!this.info.code) {
               this.info.code = this.info
             }
-            this.qrImgUrl = server.THUMBNAIL_QR + this.info.code.batch.batchNo + '/' + this.qrCodeId + '.png'
-            this.qrRecord = '养护记录'
-            this.routeUrl = '/jobGroup/groupRecord?codeId=' + this.qrCodeId
-
             let singles = response.data.resultMsg.singles
             this.singles.alias = []
             for (let key in singles) {
               this.singles.alias.push(singles[key]['alias'])
             }
           } else if (this.type === 'EQUIPMENT') {
-            this.topTitle = this.info.alias
-            this.qrRecord = '领用记录'
-            this.routeUrl = '/jobGroup/record?codeId=' + this.qrCodeId
-            this.qrImgUrl = server.THUMBNAIL_QR + this.info.batch.batchNo + '/' + this.qrCodeId + '.png'
           } else {
             this.qrRecord = ''
-            this.topTitle = this.info.alias
-            this.qrImgUrl = server.THUMBNAIL_QR + this.info.batch.batchNo + '/' + this.qrCodeId + '.png'
           }
-
-          //  server.THUMBNAIL_API
+          this.qrImgUrl = server.THUMBNAIL_QR + this.info.code.batch.batchNo + '/' + this.qrCodeId + '.png'
           this.editable = response.data.resultMsg.editable
-        } else {
-          if (response.data.resultCode === 'ERROR') {
-            this.$q.dialog({
-              title: '提示',
-              message: response.data.resultMsg.hint
-            })
-          } else {
-            this.$q.dialog({
-              title: '提示',
-              message: response.data.resultMsg
-            })
-          }
         }
       })
     }
@@ -168,49 +140,28 @@ export default {
 
 <style lang='scss'>
 @import "../../assets/css/common";
-#detail {
-  .top-field {
-    margin-top: 20px;
+#qr-detail {
+  .q-tab-label {
+    padding-bottom: 5px;
+    color: #666;
+    opacity: 1;
+    width: 100%;
   }
-  .qr-img {
-    width: 124px;
-    height: 124px;
-    padding: 2px;
-    border: 1px solid #bbbbbb;
-    margin-bottom: 10px;
+  .active .q-tab-label {
+    border-bottom: 2px solid;
+    color: #239f5b;
   }
-  .underline {
-    border-bottom: 1px solid #cccccc;
-    margin-top: 20px;
-  }
-  .top-field {
-    p {
-      margin-bottom: 3px;
-    }
-  }
-  .param {
-    span {
-      display: inline-block;
-      padding: 0px 4px;
-    }
+  .q-tab {
+    padding-left: 0px !important;
+    padding-right: 0px !important;
   }
   .qr-info {
-    margin-top: 15px;
-    margin-bottom: 30px;
     p {
       margin-bottom: 5px;
       font-size: 14px;
       color: #333333;
       line-height: 23px;
     }
-  }
-  .address {
-    margin-top: 12px;
-    margin-bottom: 12px;
-  }
-  .qr-btn {
-    margin-bottom: 30px;
-    padding: 10px 5px;
   }
   .pic-field img {
     height: auto;
