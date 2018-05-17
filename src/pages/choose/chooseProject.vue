3d77c6d6b2b54c5b40d89c8d98be234d32853dde
luyang <lu.yang.end@gmail.com>
2018-05-17 20:27:31 +0800

    q-infinite-scroll元素的mixin

--- src/pages/choose/chooseProject.vue
+++ src/pages/choose/chooseProject.vue
@@ -10,7 +10,7 @@
       </q-toolbar>
     </q-toolbar>
     <q-list id="choose-project">
-      <q-infinite-scroll :handler="load" class="scroll-field">
+      <q-infinite-scroll :handler="load" class="scroll-field" ref="scroll">
         <q-item multiline  v-for="item in list"
                 :key="item.id" class="list-list"  @click.native="toDetail(item)">
           <q-item-side class="add-btn" />
@@ -31,17 +31,18 @@
 
 <script>
   import { request } from '../../common'
+  import InfiniteScroll from '../../mixin/InfiniteScroll'
 
   export default {
-    data () {
-      return {
-        list: '',
-        loading: false,
-        pageNo: 1,
-        hasLoadAll: false,
-        urlName: '/qcode/edit?id=' + localStorage.getItem('qrCodeId') + '&typeKey=' + localStorage.getItem('typeKey')
-      }
+   
+    mixins: [
+      InfiniteScroll
+    ],
+    mounted () {
+      this.apiUrl = 'project/list?'
+      this.scroll = this.$refs.scroll
     },
+  
     methods: {
       back () {
         this.$router.goBack()
@@ -51,41 +52,6 @@
         localStorage.setItem('choose-project', JSON.stringify(item))
         this.$router.goBack()
         // return 'qcode/List?projectId' + projectId
-      },
-      async load (index, done) {
-        let that = this
-        setTimeout(() => {
-          if (!this.hasLoadAll) {
-            this.loading = true
-            request(
-              'project/list?pageNo=' + that.pageNo + '&pageSize=20',
-              'get',
-              '',
-              'json',
-              true
-            ).then(response => {
-              if (response.data.resultCode === 'SUCCESS') {
-                this.loading = false
-                let list = response.data.resultMsg
-                if (list.length === 0 || !list.length) {
-                  that.hasLoadAll = true
-                  return
-                }
-                if (list.length < 20) {
-                  that.hasLoadAll = true
-                } else {
-                  that.pageNo++
-                }
-                if (that.list.length > 0) {
-                  that.list = that.list.concat(list)
-                } else {
-                  that.list = list
-                }
-                done()
-              }
-            })
-          }
-        }, 100)
       }
     }
   }
