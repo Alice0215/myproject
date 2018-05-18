61169c247117a06020e0ba63c721e765610d9deb
Faye <muyf@shinetechchina.com>
2018-05-18 14:47:03 +0800

    二维码单株植物信息页添加

---
+++ src/pages/QrCode/Information/QrSingleInfo.vue
@@ -0,0 +1,125 @@
+<template>
+  <q-layout view="hHh lpr fFf" id="single_info">
+    <q-page-container>
+      <q-page>
+        <q-list>
+          <q-item>
+            <div class="title">二维码编号：</div>
+            <div class="ml-20 content">{{ qrCode.identifier }}</div>
+            <q-item-side  class="active btn-right" right v-if="qrCode.editable"  @click.native="edit">修改</q-item-side>
+          </q-item>
+          <q-item>
+            <div class="title">植物名称：</div>
+            <div class="ml-20 content" v-line-clamp:20="1">
+              {{ qrCode.alias }}
+            </div>
+          </q-item>
+          <q-item>
+            <div class="title">所属项目：</div>
+            <div class="ml-20 content" v-line-clamp:20="1">
+              {{ qrCode.project? qrCode.project.projectName : "" }}
+            </div>
+          </q-item>
+          <q-item>
+            <div class="title">地址：</div>
+            <div class="ml-20 content" v-line-clamp:20="1">
+              {{ qrCode.location? qrCode.location.formattedAddress : ""  }}
+            </div>
+          </q-item>
+          <q-item>
+            <div class="title">所属片区：</div>
+            <div class="ml-20 content" v-line-clamp:20="1">{{ qrCode.alias }}</div>
+          </q-item>
+          <q-item>
+            <div class="title">苗木分类：</div>
+            <div class="ml-20 content" v-line-clamp:20="1">{{ detail.category.name }}</div>
+          </q-item>
+          <q-item>
+            <div class="title">苗木面积：</div>
+            <div class="ml-20 content" v-line-clamp:20="1">{{ qrCode.alias }}</div>
+          </q-item>
+          <q-item>
+            <div class="title">苗木规格：</div>
+            <div class="ml-20 content" v-line-clamp:20="1">
+              <span class="ib pr-5">胸径：{{ detail.xiongJing }}cm</span>
+              <span  class="ib pr-5">高度：{{ detail.gaoDu }}cm</span>
+              <span  class="ib pr-5">冠幅：{{ detail.guanFu }}cm</span>
+            </div>
+          </q-item>
+          <q-item>
+            <div class="title">苗源地：</div>
+            <div class="ml-20 content" v-line-clamp:20="1">{{ detail.source }}</div>
+          </q-item>
+          <q-item>
+            <div class="title">苗木商信息：</div>
+            <div class="ml-20 content" v-line-clamp:20="1">{{ detail.dealer }}</div>
+          </q-item>
+          <q-item>
+            <div class="title">苗木其他信息：</div>
+            <div class="ml-20 content" v-line-clamp:20="1">{{ detail.other }}</div>
+          </q-item>
+          <q-item class="ib">
+            <div class="mb-10">备注：</div>
+            <div class="content">{{ qrCode.description }}</div>
+          </q-item>
+          <q-item class="ib">
+            <div class="mb-10">现场图片：</div>
+            <span v-for="(item, i) in thumbnails" v-bind:key="i" :class="{'ml-10': i !== 0}">
+              <img :src="item" preview-title-enable="false" :key="i" @click="preview(i)">
+            </span>
+          </q-item>
+        </q-list>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { ImagePreview } from 'vant'
+
+export default {
+  props: [
+    "detail",
+    "qrCode",
+    "previews",
+    "thumbnails"
+  ],
+  methods: {
+    preview (i) {
+      ImagePreview(this.previews, i)
+    }
+  }
+
+}
+</script>
+
+<style lang='scss'>
+  @import "../../../assets/css/_variable.scss";
+  #single_info {
+    background: $bgcolor;
+    .q-list {
+      background: white;
+      .q-item {
+        padding-top: 8px;
+      }
+    }
+    img {
+      width: 75px;
+      height: 75px;
+    }
+    .title {
+      width: 105px !important;
+      font-size: 15px !important;
+    }
+    .content {
+      width: calc(100% - 105px - 32px);
+      color: $text-highlight;
+      font-size: 15px !important;
+    }
+    .btn-right {
+      font-size: 15px;
+      position: absolute;
+      right: 16px;
+    }
+  }
+</style>
--- src/pages/QrCode/QrCodeType/QrCodeSingleDetail.vue
+++ src/pages/QrCode/QrCodeType/QrCodeSingleDetail.vue
@@ -1,16 +1,48 @@
 <template>
   <q-layout view="hHh lpr fFf" class="bg-primary" id="qr-detail">
+    <q-layout-header>
+      <q-toolbar class='nav-header bg-white'>
+        <q-item-side @click.native="$router.goBack()" left icon="keyboard arrow left" class="back-left">
+          <label>返回</label>
+        </q-item-side>
+        <q-toolbar-title class='header-title text-center'>
+          {{ qrCode.alias }}
+        </q-toolbar-title>
+        <q-item-side class="white-right" right/>
+      </q-toolbar>
+      <q-tabs inverted align="justify" no-pane-border class="tab-class">
+        <q-tab default name="single-info" slot="title" label="单株信息" />
+        <q-tab name="occupy-records" slot="title" label="养护记录" />
+        <q-tab name="patrol-records" slot="title" label="巡查记录" />
+        <q-tab-pane name="single-info" class="tab-pane-class">
+          <q-scroll-area class="scroll-height">
+            <qr-single-info
+              :qrCode="qrCode"
+              :detail="detail"
+              :thumbnails="thumbnails"
+              :previews="previews">
+            </qr-single-info>
+          </q-scroll-area>
+        </q-tab-pane>
+        <q-tab-pane name="occupy-records"></q-tab-pane>
+        <q-tab-pane name="patrol-records"></q-tab-pane>
+      </q-tabs>
+    </q-layout-header>
   </q-layout>
 </template>
 
 <script>
+  import QrSingleInfo from '../Information/QrSingleInfo'
   export default {
+    components: {
+      QrSingleInfo
+    },
     props: [
       "qrCode",
       "previews",
       "thumbnails",
       "detail"
-    ]    
+    ]
   }
 </script>
 
