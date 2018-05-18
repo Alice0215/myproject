5de6a66d4170a4a0690ba1c335fa08123579a78d
Faye <muyf@shinetechchina.com>
2018-05-18 16:25:35 +0800

    片区植物详情

--- src/pages/QrCode/QrCodeType/QrCodeAreaDetail.vue
+++ src/pages/QrCode/QrCodeType/QrCodeAreaDetail.vue
@@ -1,87 +1,51 @@
 <template>
+  <q-layout view="hHh lpr fFf" class="bg-primary" id="qr-detail">
+    <q-layout-header>
+      <q-toolbar class='nav-header bg-white'>
+        <q-item-side @click.native="$router.goBack()" left icon="keyboard arrow left" class="back-left">
+          <label>返回</label>
+        </q-item-side>
+        <q-toolbar-title class='header-title text-center'>
+          {{ qrCode.alias }}
+        </q-toolbar-title>
+        <q-item-side class="white-right" right/>
+      </q-toolbar>
+      <q-tabs inverted align="justify" no-pane-border class="tab-class">
+        <q-tab default name="area-info" slot="title" label="片区信息" />
+        <q-tab name="occupy-records" slot="title" label="养护记录" />
+        <q-tab name="patrol-records" slot="title" label="巡查记录" />
+        <q-tab-pane name="area-info" class="tab-pane-class">
+          <q-scroll-area class="scroll-height">
+            <qr-area-info
+              :qrCode="qrCode"
+              :detail="detail"
+              :thumbnails="thumbnails"
+              :previews="previews">
+            </qr-area-info>
+          </q-scroll-area>
+        </q-tab-pane>
+        <q-tab-pane name="occupy-records"></q-tab-pane>
+        <q-tab-pane name="patrol-records"></q-tab-pane>
+      </q-tabs>
+    </q-layout-header>
+  </q-layout>
 </template>
 
 <script>
-export default {
+import QrAreaInfo from '../Information/QrAreaInfo'
 
+export default {
+  components: {
+    QrAreaInfo
+  },
   props: [
     "qrCode",
     "previews",
     "thumbnails",
     "detail"
-  ]    
+  ]
 }
 </script>
 
 <style lang='scss'>
-@import "../../../assets/css/common";
-.q-popover {
-  width: 100% !important;
-}
-.time-btn {
-  width: 120px;
-  border: 1px solid #e8e8e8;
-  color: #666;
-  padding-top: 3px;
-}
-#qr-detail {
-  .auto-width {
-    min-width: auto !important;
-  }
-  .q-tab-label {
-    padding-bottom: 5px;
-    color: #666;
-    opacity: 1;
-    width: 100%;
-  }
-  .active .q-tab-label {
-    border-bottom: 2px solid;
-    color: #239f5b;
-  }
-  .q-tab {
-    padding-left: 0px !important;
-    padding-right: 0px !important;
-  }
-  .qr-info {
-    p {
-      margin-bottom: 5px;
-      font-size: 14px;
-      color: #333333;
-      line-height: 23px;
-    }
-  }
-  .pic-field img {
-    width: 23%;
-    margin-right: 1%;
-  }
-  #maintenance {
-    .q-item-main {
-      .content {
-        .work-content-title {
-          width: 75px;
-        }
-        .work-content {
-          width: calc(100% - 90px);
-        }
-        div {
-          display: flex;
-          div {
-            font-size: 15px;
-          }
-        }
-      }
-    }
-    .title {
-      font-size: 18px !important;
-      color: $text-highlight;
-    }
-    .type-title {
-      color: $type-title;
-    }
-    .btn-light {
-      color: $light-text;
-      font-size: 15px;
-    }
-  }
-}
 </style>
