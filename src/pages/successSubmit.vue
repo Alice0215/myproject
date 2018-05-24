8c4295414cd901846ce0bf785c4b3eeb5b9f6178
xtq <xuetq@shinetechchina.com>
2018-05-24 15:52:44 +0800

    修改提示错误

--- src/pages/successSubmit.vue
+++ src/pages/successSubmit.vue
@@ -3,7 +3,7 @@
     <div class="mt-12 text-center card">
       <q-icon name="check_circle" class="green-color mt-25" size="100px"/>
       <div class="success-text pv-15 font-20">完成</div>
-      <div class="hint-text font-14 color-gray">3秒后自动跳转至详情页</div>
+      <div class="hint-text font-14 color-gray">3秒后自动跳转至项目详情页</div>
     </div>
   </div>
 </template>
